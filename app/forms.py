b6629abd5429046edb7e9caf953c9ca0fbd8905d
Garvit <d.garvit@gmail.com>
2017-09-24 10:34:29 +0530

    Model for Comment

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from validators import MimetypeValidator
-from .models import Video
+from .models import Video, Comment
 
 class VideoForm(forms.ModelForm):
 	class Meta:
@@ -8,4 +8,13 @@ class VideoForm(forms.ModelForm):
 		exclude = ['user']
 		widgets = {
 			'file': forms.FileInput(attrs={'accept':'video/mp4'}),
+		}
+
+
+class CommentForm(forms.ModelForm):
+	class Meta:
+		model = Comment
+		fields = ['comment', 'file']
+		widgets = {
+			'comment': forms.TextInput(attrs={'placeholder': 'Write a Comment...'}),
 		}
